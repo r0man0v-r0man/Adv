fdcccb19ab091486e74ac645a4c31000f5a1e870
r0man0v-r0man <45589927+r0man0v-r0man@users.noreply.github.com>
2020-02-23 15:25:50 +0300

    init home page search

--- Adv.API/ClientApp/src/app/components/search/search.component.ts
+++ Adv.API/ClientApp/src/app/components/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cities } from 'src/app/models/cities';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+    /** Selected City, default district is: 0 */
+    selectedCity: number = 0;
+    /** Array of cities */
+    listOfCities: Array<{ label: string; value: number}> = [];
+    longForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.initLongForm();
+    this.setCities();
   }
 
+
+  initLongForm(){
+    this.longForm = this.formBuilder.group({
+      city: [this.selectedCity, [Validators.required]]
+    })
+  }
+    /**
+   * Set list of districts for select menu
+   */
+  setCities(){
+    this.listOfCities.push(
+      { label: 'Несвиж', value: Cities.nesvizh }
+      );
+  }
 }
