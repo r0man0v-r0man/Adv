eebdfbb94b6418d3b4fce1b2696960c2b2a706dd
r0man0v-r0man <45589927+r0man0v-r0man@users.noreply.github.com>
2020-05-16 18:07:08 +0300

    done with forms

--- Adv.API/ClientApp/src/app/models/flatRentModel.ts
+++ Adv.API/ClientApp/src/app/models/flatRentModel.ts
@@ -1,3 +1,21 @@
+import { UploadFile } from 'ng-zorro-antd/upload';
+
 export interface FlatRentModel {
-    id: number;
+    userId: string;
+    isActive: boolean;
+    images: UploadFile[];
+    address: string;
+    floor: number;
+    allFloor: number;
+    rooms: number;
+    balcony: number;
+    furniture: boolean;
+    refrigerator:  boolean;
+    microwaveOven:  boolean;
+    internet:  boolean;
+    washingMachine:  boolean;
+    price: number;
+    duration: number;
+    phone: number;
+    description: string;
 }
--- Adv.API/ClientApp/src/app/pages/create-advert/components/rent-flat/rent-flat.component.ts
+++ Adv.API/ClientApp/src/app/pages/create-advert/components/rent-flat/rent-flat.component.ts
@@ -7,6 +7,8 @@ import { SuggestService } from 'src/app/services/suggest.service';
 import { FlatRentModel } from 'src/app/models/flatRentModel';
 import { UploadFile, UploadChangeParam } from 'ng-zorro-antd/upload';
 import { Observable } from 'rxjs';
+import { DescriptionValidators } from '../../validators/description.validators';
+import { Duration } from 'src/app/models/duration';
 
 @Component({
   selector: 'rent-flat',
@@ -25,6 +27,30 @@ export class RentFlatComponent implements OnInit {
   allFloor: number;
   /** кол-во комнат */
   rooms: number;
+  /** балкон */
+  selectedBalcony: number = 1;
+  listOfBalcony: Array<{ label: string, value: number }> = [];
+  /** мебель */
+  furniture: boolean = false;
+  /** холодильник */
+  refrigerator: boolean = false;
+  /** микроволновая печь */
+  microwaveOven: boolean = false;
+  /** интернет */
+  internet: boolean = false;
+  /** стиральная машина */
+  washingMachine: boolean = false;
+  /** цена */
+  price: number = 30000;
+  formatterDollar = (value: number) => `$ ${value}`;
+  parserDollar = (value: string) => value.replace('$ ', '');
+  /** тип аренды */
+  selectedDuration: number = 0;
+  listOfDuration: Array<{ label: string; value: number}> = [];
+  /** телефон */
+  phone: number = 80291234567;
+  /** описание */
+  description: string = '';
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
@@ -38,6 +64,8 @@ export class RentFlatComponent implements OnInit {
     this.initForm();
   }
   initForm(){
+    this.setListOfBalcony();
+    this.setDurations();
     this.flatRentForm = this.formBuilder.group({
       userId:[ this.authService.currentUser.sub,[Validators.required]],
       isActive: [ true ],
@@ -46,6 +74,16 @@ export class RentFlatComponent implements OnInit {
       floor: [ this.floor, [Validators.required]],
       allFloor: [ this.allFloor, [Validators.required]],
       rooms: [ this.rooms, [Validators.required]],
+      balcony: [ this.selectedBalcony, [Validators.required]],
+      furniture: [this.furniture],
+      refrigerator: [ this.refrigerator],
+      microwaveOven: [ this.microwaveOven],
+      internet: [ this.internet],
+      washingMachine: [ this.washingMachine],
+      price: [ null, [Validators.required]],
+      duration: [ this.selectedDuration, [Validators.required]],
+      phone: [ this.phone, [Validators.required, Validators.pattern("[0-9]*")]],
+      description: [ null, [DescriptionValidators.notOnlySpace]]
     })
   }
 
@@ -82,6 +120,20 @@ export class RentFlatComponent implements OnInit {
         });
       }
     })
+  }
+  /** установка наличия балкона */
+  setListOfBalcony(){
+    this.listOfBalcony.push(
+      { label: 'Есть', value: 1 },
+      { label: 'Нет', value: 0 }
+    )
+  }
+  /** установка списка типов аренды */
+  setDurations(){
+    this.listOfDuration.push(
+      { label: 'Длительная', value: Duration.long },
+      { label: 'Часы/сутки', value: Duration.short }
+      );
   }
     /**
    * установка значения для поля формы
