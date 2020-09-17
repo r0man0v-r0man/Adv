f73db7e1dacda05aa6f3ceeb63dfaa4a57ebeb72
Романов Роман <romanovmailbox@mail.ru>
2020-09-17 19:03:27 +0300

    add robots.txt controller

---
+++ Adv.API/Controllers/RobotsTxtController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Adv.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RobotsTxtController : ControllerBase
+    {
+        [Route("/robots.txt")]
+        public string RobotsTxt()
+        {
+            return
+                "User-agent: *\r\nDisallow: /login\r\nDisallow: /register\r\nDisallow: /create-advert\r\nSitemap: https://halupa.by/sitemap.xml";
+        }
+    }
+}
